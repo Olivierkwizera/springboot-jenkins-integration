f21dc00f907459453d2a9d0d5f9ac5842c5a5bed
Olivier KWIZERA <olivier.kwizera@emeal.nttdata.com>
2024-01-29 08:40:11 +0100

    Initial commit

---
+++ src/test/java/com/ci/cd/springboot/jenkins/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.ci.cd.springboot.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+    public static Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing...");
+    }
+
+
+}
